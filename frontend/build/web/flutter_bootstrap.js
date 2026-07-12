354e8149d892960f0fbb4b004d54dcb59e730787
Victor kirimi <victorkirimi01@gmail.com>
2026-07-12 21:16:22 +0300

    Rebuild Flutter web with employee list fix

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2998644654" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "920677111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
