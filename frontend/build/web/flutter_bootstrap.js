fed30bf75b7ed57cee8694882c4a235aa8450efa
Victor kirimi <victorkirimi01@gmail.com>
2026-07-14 18:43:30 +0300

    build: add flutter web build with customer management

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "920677111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2252030375" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
